bed9b5ef1d5be33d056cba3359f91918606567f5
BadiRodriguez <badirodriguez.csci@gmail.com>
2023-11-14 18:52:34 -0500

    General upgrades

--- Player.cpp
+++ Player.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "Player.h"
+
+using namespace std;
+
+Player::Player(const std::string& playerName) {
+    // Initialize player attributes in the constructor
+    name = playerName;
+    energyCount = 8; // Starting energy count (by rule, it is 8)
+    health = 20; // Starting health (by rule, it is 20)
+    // Assuming you have a deck of cards, and you want to initialize it in the constructor
+    // You can adjust the initialization based on your actual implementation of the Card class
+    deck.push_back(Card("Card1", 2, "Description1", "Type1", 3, 2));
+    deck.push_back(Card("Card2", 1, "Description2", "Type2", 2, 1));
+    // ... Add more cards to the deck as needed
+}
+
+void Player::drawCard() {
+
+    if (!deck.empty()) {
+        // Draw the top card from the deck
+        Card drawnCard = deck.back();
+
+        // Remove the drawn card from the deck
+        deck.pop_back();
+
+        // Add the drawn card to the player's hand
+        hand.push_back(drawnCard);
+    }
+    // You might want to handle the case when the deck is empty
+    else {
+        throw std::out_of_range("Cannot draw a card. The deck is empty! This is your last stand!");
+    }
+}
--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main() {
     // Usage example:
     CardDeck myDeck;
 
-    myDeck.addCard(Card("Proto-Squire", 3, "A digital tin-knight, small but sturdy", "Block"));
-    myDeck.addCard(Card("FlossBush", 2, "A flowering spawn of nature, bites!", "Sentry"));
+    myDeck.addCard(Card("Proto-Squire", 3, "A digital tin-knight, small but sturdy", "Block", 0, 4));
+    myDeck.addCard(Card("FlossBush", 2, "A flowering spawn of nature, bites!", "Sentry", 1, 1));
 
     myDeck.displayDeck();
 
