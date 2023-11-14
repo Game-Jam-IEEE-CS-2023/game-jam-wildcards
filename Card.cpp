5760779c79b1927d2358ca29d239a1a17cee4a90
BadiRodriguez <badirodriguez.csci@gmail.com>
2023-11-14 18:27:38 -0500

    Modification of the Card.cpp file and the Card.h header to upgrade the displayCard method and add attack/defense functionalities and properties.

--- Card.cpp
+++ Card.cpp
@@ -7,15 +7,22 @@
 #include <iostream>
 #include <string>
 #include "Card.h"
+#include <iomanip>
 
 using namespace std; // Help
 
 // Define class methods of Card - (Main runs).
-Card::Card(string _cardName, int _cardCost, string _cardDesc, string _cardType) {
+Card::Card(string _cardName, int _cardCost, string _cardDesc, string _cardType, int _cardAtk, int _cardDef) {
     cardName = _cardName;
     cardCost = _cardCost;
     cardDesc = _cardDesc;
     cardType = _cardType;
+    cardAtk = _cardAtk;
+    cardDef = _cardDef;
+}
+
+string Card::getCardName() {
+    return cardName;
 }
 
 int Card::getCardCost() {
@@ -31,16 +38,38 @@ string Card::getCardType() {
 }
 
 void Card::displayCard() {
+    const int cardWidth = 40; // Adjustable
     string orbSymbol = "0";
-    cout << "[" << cardName << "]" << endl;
-    cout << "Cost: ";
 
+    // Display horizontal line
+    cout << setfill('-') << setw(cardWidth) << "\n" << setfill(' ');
+
+    // Display card name
+    cout << "[" << setw(cardWidth - 2) << left << cardName << "]" << endl;
+
+    // Display cost
+    cout << "Cost: " << setw(cardWidth - 7) << right;
     for (int i = 0; i < getCardCost(); ++i) {
         cout << orbSymbol;
     }
+    cout << "|\n";
 
-    cout << "\nType: [" << cardType << "]\n";
-    cout << "\nDesc: " << cardDesc << "\n";
+    // Display type
+    cout << "Type: [" << setw(cardWidth - 9) << left << cardType << "]\n";
+
+    // Display description
+    cout << "Desc: " << setw(cardWidth - 6) << left << cardDesc << "\n";
+
+    // Display horizontal line
+    cout << setfill('-') << setw(cardWidth) << "\n" << setfill(' ');
+}
+
+int Card::getCardAtk() {
+    return cardAtk;
+}
+
+int Card::getCardDef() {
+    return cardDef;
 }
 
 
